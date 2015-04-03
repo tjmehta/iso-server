4bf31062d8c9d247cb703ec80ed97a886052502b
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-04-02 18:32:06 -0700

    init

---
+++ server/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var makeQueryRequest = require('make-query-request');
+var app = express();
+
+app.get('/', function (req, res, next) {
+  res.send('yo');
+});
+
+app.use('/client', express.static('client'));
+
+app.get('/server', function (req, res, next) {
+  makeQueryRequest(req, res);
+});
+
+app.post('/query', function (req, res, next) {
+  res.json({
+    hello: "hello",
+    body : req.body,
+    query: req.query
+  });
+});
+
+app.listen(3030);
