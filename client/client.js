8359f60abc739a6c767a017b1a69a4e128764845
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-04-03 15:57:15 -0700

    simplification

--- client/client.js
+++ client/client.js
@@ -1,1 +1,23 @@
-require('../lib/make-query-request')();
+var qs = require('querystring');
+var makeRequest = require('make-request');
+
+try {
+  var query = qs.parse(window.location.search.slice(1));
+  makeRequest(query, function (err, bodyStr) {
+    if (err) { return handleErr(err); }
+    writeBody(bodyStr);
+  });
+}
+catch (err) {
+  handleErr(err);
+}
+
+function handleErr (err) {
+  console.log('handleErr', arguments);
+  writeBody('Error: '+err.message);
+}
+
+function writeBody (str) {
+  console.log('writeBody', arguments);
+  document.body.innerHTML = str;
+}
